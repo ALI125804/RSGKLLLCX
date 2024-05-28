2f71831bd40eed4a72a469352a66fc4bab41b46a
ALI125804 <qwhckkhgfhzdd@gmail.com>
2024-05-28 07:17:34 +0300

    Update DEC_ERROR.py

--- DEC_ERROR.py
+++ DEC_ERROR.py
@@ -12,29 +12,6 @@ print('\n\033[1;37 حسن يرحب بكم 😔...')
 
 
 
-def LONE():
-  uuid = str(os.geteuid()) + str(os.getlogin())
-  id = "-".join(uuid)
-  print("\033[2;36m; ID : "+id)
-  try:
-    httpCaht = requests.get("https://github.com/ALI125804/RSGKLLLCX/blob/main/Approval.txt").text
-    if id in httpCaht:
-      print('')
-      print("""\033[1;92m 𝗔𝗰𝘁𝗶𝘃𝗮𝘁𝗶𝗼𝗻 𝗰𝗼𝗺𝗽𝗹𝗲𝘁𝗲𝗱 𝘀𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 """)
-      msg = str(os.geteuid())
-      time.sleep(1)
-      pass
-    else:
-      print("Done")
-      print("\033[2;36m 𝗔𝗰𝘁𝗶𝘃𝗮𝘁𝗶𝗼𝗻 𝗰𝗼𝗺𝗽𝗹𝗲𝘁𝗲𝗱 𝘀𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 @TTL9T ")
-      
-      time.sleep(1)
-      sys.exit()  
-  except:
-    sys.exit()
-    
-    LONE()
-LONE()
 
 try:
         import os,requests,json,time,re,random,sys,uuid,string,subprocess
